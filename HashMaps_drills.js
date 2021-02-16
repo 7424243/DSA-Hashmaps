c27c6a9c0030c77d5252ac44f1d1f7f7e72f4fbe
Sam Sheets <sam.sheets94@gmail.com>
2021-02-16 11:46:33 -0700

    completed problems 1-3

---
+++ HashMaps_drills.js
@@ -0,0 +1,96 @@
+const {HashMap} = require('./hashmap')
+
+/* ===== 1. Create a HashMap class ===== 
+Create a .js file called HashMaps_drills. In the file import the HashMap module. 
+Create a function called main()
+Inside your main() function, create a hash map called lotr.
+For your hash map that you have created, set the MAX_LOAD_RATIO = 0.5 and SIZE_RATIO = 3.
+Add the following items to your hash map: {"Hobbit": "Bilbo"}, {"Hobbit": "Frodo"},
+{"Wizard": "Gandalf"}, {"Human": "Aragorn"}, {"Elf": "Legolas"}, {"Maiar": "The Necromancer"},
+{"Maiar": "Sauron"}, {"RingBearer": "Gollum"}, {"LadyOfLight": "Galadriel"}, {"HalfElven": "Arwen"},
+{"Ent": "Treebeard"}
+*/
+HashMap.MAX_LOAD_RATIO = 0.5
+HashMap.SIZE_RATIO = 3
+
+function main() {
+    const lotr = new HashMap()
+    lotr.set('Hobbit', 'Bilbo')
+    lotr.set('Hobbit', 'Frodo')
+    lotr.set('Wizard', 'Gandalf')
+    lotr.set('Human', 'Aragorn')
+    lotr.set('Elf', 'Legolas')
+    lotr.set('Maiar', 'The Necromancer')
+    lotr.set('Maiar', 'Sauron')
+    lotr.set('RingBearer', 'Gollum')
+    lotr.set('LadyOfLight', 'Galadriel')
+    lotr.set('HalfElven', 'Arwen')
+    lotr.set('Ent', 'Treebeard')
+    console.log(lotr)
+    //length: 9
+    //my length is less than the number of key/values that I set because some of the keys were not unique which just replaced the values.
+    console.log(lotr.get('Maiar'))//output: 'Sauron'. This is the most recent value set for the key of 'Maiar'
+    console.log(lotr.get('Hobbit'))//output: 'Frodo'. This is the most recent value set for the key of 'Hobbit'
+    //capacity: 24 (We set more than 4 key/value pairs (8(capacity) * 50%) so 8(capacity) * 3(size_ratio) = 24)
+}
+main()
+
+/* ===== 2. WhatDoesThisDo =====
+DO NOT run the following code before solving the problem.
+What is the output of the following code? explain your answer.
+const WhatDoesThisDo = function(){
+
+    //create a new hashmap and set 2 key/value pairs
+    let str1 = 'Hello World.';
+    let str2 = 'Hello World.';
+    let map1 = new HashMap();
+    map1.set(str1,10);
+    map1.set(str2,20);
+
+    //create a second new hashmap and set 2 key/value pairs
+    let map2 = new HashMap();
+    let str3 = str1;
+    let str4 = str2;
+    map2.set(str3,20);
+    map2.set(str4,10);
+
+    //print out str1 key/value from the first hashmap
+    console.log(map1.get(str1));//output: "Hello World.": 20 >> str1 & str2 are the same, so the value will be the most recent assignment
+
+    //print out str3 key/value from the second hashmap
+    console.log(map2.get(str3));//output: "Hello World.": 10 >> str3 & str4 are the same, so the valeu will be the most recent assignment
+}
+*/
+
+/* ===== 3. Demonstrate understanding of Hash maps =====
+*You don't need to write code for the following two drills. use any drawing app or simple pen and paper *
+
+1) Show your hash map after the insertion of keys 10, 22, 31, 4, 15, 28, 17, 88, 59 into a hash map of length 11 using open addressing and a hash function k mod m, where k is the key and m is the length.           
+10 mod 11 = 1
+22 mod 11 = 0
+31 mod 11 = 9
+4 mod 11 = 4
+15 mod 11 = 4
+28 mod 11 = 6
+17 mod 11 = 6
+88 mod 11 = 0
+59 mod 11 = 4
+i: 0   1   2   3   4   5   6   7   8   9   10
+k: 22  10  88 Null 4   15  28  17  59  null 31        
+
+2) Show your hash map after the insertion of the keys 5, 28, 19, 15, 20, 33, 12, 17, 10 into the hash map with collisions resolved by separate chaining. Let the hash table have a length m = 9, and let the hash function be k mod m.
+5 % 9 = 5
+28 % 9 = 1
+19 % 9 = 1
+15 % 9 = 6
+20 % 9 = 2
+33 % 9 = 6
+12 % 9 = 3
+17 % 9 = 8
+10 % 9 = 1
+i: 0   1   2   3   4   5   6   7   8   
+k:null     20  12 null 5      null 17  
+       28                  15
+       19                  33
+       10
+*/
